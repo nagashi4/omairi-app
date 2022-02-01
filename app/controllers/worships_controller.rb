81e71053702d7342d164cfd6f8a7e44eac68169b
nagashi4 <92012577+nagashi4@users.noreply.github.com>
2022-02-01 18:34:11 +0900

    newアクションとcreateアクションの定義

--- app/controllers/worships_controller.rb
+++ app/controllers/worships_controller.rb
@@ -3,7 +3,16 @@ class WorshipsController < ApplicationController
     @worships = Worship.all
   end
 
+  def new
+    @worship = Worship.new
+  end
+  
+  def create
+    Worship.create(worship_params)
+  end
 
-  
-  
+  private
+  def worship_params
+    params.require(:worship).permit(:name, :image, :text)
+  end
 end
--- app/models/worship.rb
+++ app/models/worship.rb
@@ -1,3 +1,3 @@
 class Worship < ApplicationRecord
-  
+  validates :text, presence: true
 end
